41373bce6b9af379276e6f51b5950b80204e6e6d
Charles Thierry <CharlesThierry@users.noreply.github.com>
2021-02-26 17:48:45 +0100

    Purge the test from the coredata storage

--- LBC Test/LBC TestTests/CoreDataTests.swift
+++ LBC Test/LBC TestTests/CoreDataTests.swift
@@ -21,6 +21,7 @@ class CoreDataTests: XCTestCase {
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        manager?.purge()
     }
 
     func testPopulateCategories() {
