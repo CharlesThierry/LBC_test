033c1d170de640fad065cbc749b080052702bbd7
charlesthierry <CharlesThierry@users.noreply.github.com>
2021-02-27 12:01:02 +0100

    Rename file

---
+++ LBC Test/LBC Test/Constants/DateFormatter+Custom.swift
@@ -0,0 +1,19 @@
+//
+//  DateFormatter+Supported.swift
+//  Test LBC
+//
+//  Created by Charles Thierry on 27/02/2021.
+//
+
+import Foundation
+
+extension DateFormatter {
+    static let custom: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+        formatter.calendar = Calendar(identifier: .gregorian)
+        formatter.timeZone = TimeZone(secondsFromGMT: 0)
+        formatter.locale = Locale(identifier: "en_US_POSIX")
+        return formatter
+    }()
+}
