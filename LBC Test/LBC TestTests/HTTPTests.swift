5b76997b9951498c1bd4de9b021348df411de734
Charles Thierry <CharlesThierry@users.noreply.github.com>
2021-02-27 08:10:39 +0100

    Add test to check description creation

--- LBC Test/LBC TestTests/HTTPTests.swift
+++ LBC Test/LBC TestTests/HTTPTests.swift
@@ -36,4 +36,23 @@ class HTTPTests: XCTestCase {
         wait(for: [expCategory], timeout: 10.0)
     }
 
+    func testJSONConversion() {
+        let string = "[{\"id\": 1461267313,\"category_id\":4,\"title\":\"Title\",\"description\": \"Description\",\"price\":140.0,\"images_url\":{\"small\":\"https://raw.githubusercontent.com/leboncoin/paperclip/master/ad-small/2c9563bbe85f12a5dcaeb2c40989182463270404.jpg\",\"thumb\":\"https://raw.githubusercontent.com/leboncoin/paperclip/master/ad-thumb/2c9563bbe85f12a5dcaeb2c40989182463270404.jpg\"},\"creation_date\":\"2019-11-05T15:56:59+0000\",\"is_urgent\":false,\"siret\":\"123 123 123\"}]"
+        let data = Data(string.utf8)
+
+        let description = generateItemsDescriptions(data: data, type: [ClassifiedDescription].self)
+        switch description {
+        case .failure(let error):
+            XCTFail("Couldn't convert the json data \(error)")
+            return
+        case .success(let description):
+            XCTAssert(description[0].categoryID == 4, "Fail categoryID")
+            XCTAssert(description[0].id == 1461267313, "Fail id")
+            XCTAssert(description[0].title == "Title", "Fail title")
+            XCTAssert(description[0].description == "Description", "Fail description")
+            XCTAssert(description[0].price == 140.0, "Fail price")
+            XCTAssert(description[0].urgent == false, "Fail urgent")
+            XCTAssert(description[0].siret == "123 123 123", "Fail siret")
+        }
+    }
 }
