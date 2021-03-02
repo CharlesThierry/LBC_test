b588e2203ce4ec74ae8f7198fbf790bd99e1e868
charlesthierry <CharlesThierry@users.noreply.github.com>
2021-03-02 15:38:11 +0100

    Add category picker view

--- LBC Test/LBC Test/UI/CategoryPicker.swift
+++ LBC Test/LBC Test/UI/CategoryPicker.swift
@@ -11,32 +11,41 @@ class CategoryPickerViewController: UIViewController, UIPickerViewDelegate, UIPi
     weak var results: FetchResults?
     
     internal var picker = UIPickerView(frame: .zero)
-    internal var button : UIButton = {
+    internal var clearButton: UIButton = {
         let btn = UIButton(frame: .zero)
         btn.translatesAutoresizingMaskIntoConstraints = false
         btn.setTitle("Toutes categories", for: .normal)
         btn.setTitleColor(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1), for: .normal)
         return btn
     }()
+
+    internal var confirmButton: UIButton = {
+        let btn = UIButton(frame: .zero)
+        btn.translatesAutoresizingMaskIntoConstraints = false
+        btn.setTitle("OK", for: .normal)
+        btn.setTitleColor(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1), for: .normal)
+        return btn
+    }()
     
     override func loadView() {
         view = UIView(frame: .zero)
-        view.clipsToBounds = true
-        view.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        
+        let underview = UIView(frame: .zero)
+        underview.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        underview.layer.cornerRadius = 5.0
+        view.addSubview(underview)
+        underview.setBasicConstraints(top: nil, bottom: view.bottomAnchor, left: view.leadingAnchor, right: view.trailingAnchor)
+        
         picker.translatesAutoresizingMaskIntoConstraints = false
         picker.delegate = self
         picker.dataSource = self
-        view.addSubview(picker)
-        view.addSubview(button)
         
-        picker.bottomAnchor.constraint(equalTo: button.topAnchor).isActive = true
-        picker.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-        picker.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-        
-        button.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-        button.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-        button.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
-
+        underview.addSubview(picker)
+        underview.addSubview(clearButton)
+        underview.addSubview(confirmButton)
+        picker.setBasicConstraints(top: underview.topAnchor, bottom: nil, left: underview.leadingAnchor, right: underview.trailingAnchor)
+        clearButton.setBasicConstraints(top: picker.bottomAnchor, bottom: nil, left: underview.leadingAnchor, right: underview.trailingAnchor)
+        confirmButton.setBasicConstraints(top: clearButton.bottomAnchor, bottom: underview.bottomAnchor, left: underview.leadingAnchor, right: underview.trailingAnchor)
     }
     
     func pickerView(_: UIPickerView, titleForRow row: Int, forComponent _: Int) -> String? {
@@ -62,7 +71,7 @@ class CategoryPickerViewController: UIViewController, UIPickerViewDelegate, UIPi
         return res.numberOfCategories
     }
     
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+    func pickerView(_: UIPickerView, didSelectRow row: Int, inComponent _: Int) {
         results?.setCategoryFilter(categoryID: results?.category(at: row)?.id)
     }
 }
